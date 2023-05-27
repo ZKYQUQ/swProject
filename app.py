9040394e50c3a5ce683d107a935306565ee4a9e3
ZKYQUQ <1580062696@qq.com>
2023-05-27 15:20:52 +0800

    server requirement

--- app.py
+++ app.py
@@ -179,6 +179,7 @@ def download():
 
 if __name__ == '__main__':
     # app.run(host="0.0.0.0", port=5000)
+    # app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
 
     app.run(debug=True)
     # app.run(debug=True, host="0.0.0.0", port=8000)
--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 # 数据库类型+数据库操作引擎://用户名:密码@主机名:端口/数据库名
-db_url = "mysql+pymysql://root:xxx@127.0.0.1:3306/test_db"
+# db_url = "mysql+pymysql://root:xxx@127.0.0.1:3306/test_db"
+db_url = "mysql+pymysql://test:123456@boldwinds.top:3306/sw_db"
 requests_proxy = ""
 max_upload_size = 24 * 1024 * 1024
 project_path = "D:\\pycharm\\swProject\\"
 ics_save_path = project_path + "save\\ics"
 json_save_path = project_path + "save\\json"
-ics_url_prefix = "http://localhost:5000/download?type=ics&filename="
+# ics_url_prefix = "http://localhost:5000/download?type=ics&filename="
+ics_url_prefix = "http://boldwinds.top:5000/download?type=ics&filename="
 psw_js_path = project_path + "EncryptPassword.js"
