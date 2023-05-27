68e225de54b66aab290fab761b8a8d8c47523443
ZKYQUQ <1580062696@qq.com>
2023-05-27 15:27:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # 数据库类型+数据库操作引擎://用户名:密码@主机名:端口/数据库名
-# db_url = "mysql+pymysql://root:xxx@127.0.0.1:3306/test_db"
-db_url = "mysql+pymysql://test:123456@boldwinds.top:3306/sw_db"
+db_url = "mysql+pymysql://root:xxx@127.0.0.1:3306/test_db"
 requests_proxy = ""
 max_upload_size = 24 * 1024 * 1024
 project_path = "D:\\pycharm\\swProject\\"
